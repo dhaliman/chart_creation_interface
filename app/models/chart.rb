2a18dcd1fde8d1c6f2fdda27c938f03cb645a967
Manmeet Singh Dhaliwal <dhaliwalsinghmanmeet@gmail.com>
2017-08-31 16:28:05 +0530

    Add validation of new chart form

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,8 +1,8 @@
 class Chart < ApplicationRecord
-  # validates :type, presence: true
-  # validates :title, presence: true
-  # validates :url, presence: true
-  
+  validates :type_of, presence: true
+  validates :title, presence: true
+  validates :url, presence: true
+
   has_many :series
   has_one :x_axis
 end
